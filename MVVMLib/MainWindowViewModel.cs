6da2fafcad0a03ad07393dc83c6c6e70f9653a87
VarChar42 <michw2014@gmail.com>
2022-04-22 17:48:28 +0200

    Init

---
+++ MVVMLib/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MVVM.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMLib
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private string _title { get; set; } = "HELLLO";
+
+        public string Title
+        {
+            get { return _title; } 
+            set
+            {
+                _title = value;
+
+                RaisePropertyChangedEvent(nameof(Title));
+            }
+        }
+
+    }
+}
